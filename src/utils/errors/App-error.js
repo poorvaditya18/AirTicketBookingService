59b91807d5773ae0a0242d4502ff834af500cfcc
poorvaditya18 <aditya.behre@gmail.com>
2022-12-26 13:21:53 +0530

    Added Error Files

---
+++ src/utils/errors/App-error.js
@@ -0,0 +1,13 @@
+// Generic Error File
+
+class AppError extends Error {
+  constructor(name, message, explanation, statusCode) {
+    super();
+    this.name = name;
+    this.message = message;
+    this.explanation = explanation;
+    this.statusCode = statusCode;
+  }
+}
+
+module.exports = AppError;
---
+++ src/utils/errors/service-error.js
@@ -0,0 +1,16 @@
+const { StatusCodes } = require("http-status-codes");
+
+class ServiceError extends Error {
+  constructor(
+    message = "Something Went Wrong",
+    explanation = "Service Layer Error",
+    statusCode = StatusCodes.INTERNAL_SERVER_ERROR
+  ) {
+    this.name = "ServiceError";
+    this.message = message;
+    this.explanation = explanation;
+    this.statusCode = statusCode;
+  }
+}
+
+module.exports = ServiceError;
