ccca96bb3a7e04243d4f0b578b55cb9fe9265306
poorvaditya behre <aditya.behre@gmail.com>
2023-02-07 21:38:53 +0530

    added Rabbitmq Config

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -5,4 +5,7 @@ dotenv.config();
 module.exports = {
   PORT: process.env.PORT,
   FLIGHT_SERVICE_PATH: process.env.FLIGHT_SERVICE_PATH,
+  EXCHANGE_NAME: process.env.EXCHANGE_NAME,
+  REMINDER_BINDING_KEY: process.env.REMINDER_BINDING_KEY,
+  MESSAGE_BROKER_URL: process.env.MESSAGE_BROKER_URL,
 };
--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,29 +1,41 @@
 const { StatusCodes } = require("http-status-codes");
 const { BookingService } = require("../services/index");
-
+const { createChannel, publishMessage } = require("../utils/messageQueue");
+const { REMINDER_BINDING_KEY } = require("../config/serverConfig");
 const bookingService = new BookingService();
 
-const create = async (req, res) => {
-  try {
-    const response = await bookingService.createBooking(req.body);
-    console.log("Response from Booking Controller", response);
-    return res.status(StatusCodes.OK).json({
-      message: "Succesfully Completed Booking",
-      success: true,
-      err: {},
-      data: response,
-    });
-  } catch (error) {
-    console.log("From Booking Controller", error);
-    return res.status(error.statusCode).json({
-      message: error.message,
-      success: false,
-      err: error.explanation,
-      data: {},
+class BookingController {
+  constructor() {}
+
+  async sendMessageToQueue(req, res) {
+    const channel = await createChannel();
+    const data = { message: "Success" };
+    publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+    return res.status(200).json({
+      message: "Succesfully published the event",
     });
   }
-};
 
-module.exports = {
-  create,
-};
+  async create(req, res) {
+    try {
+      const response = await bookingService.createBooking(req.body);
+      console.log("Response from Booking Controller", response);
+      return res.status(StatusCodes.OK).json({
+        message: "Succesfully Completed Booking",
+        success: true,
+        err: {},
+        data: response,
+      });
+    } catch (error) {
+      console.log("From Booking Controller", error);
+      return res.status(error.statusCode).json({
+        message: error.message,
+        success: false,
+        err: error.explanation,
+        data: {},
+      });
+    }
+  }
+}
+
+module.exports = BookingController;
